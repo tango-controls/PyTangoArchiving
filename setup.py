d217977d76c418f8893d6a1b5ef057638d825d8e
Sergi Rubio <srubio@cells.es>
2017-09-23 08:26:39 +0200

    update requires in setup.py

--- setup.py
+++ setup.py
@@ -92,5 +92,6 @@ setup(
     entry_points=entry_points,
     include_package_data=True,
     package_data=package_data,
+    install_requires=['fandango','PyTango','MySQL-python'],
     zip_safe=False
   )
