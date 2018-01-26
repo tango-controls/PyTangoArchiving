7b28854e744c0b43f85e82e8bd2d70e071e53b41
Sergi Rubio <srubio@cells.es>
2018-01-26 15:52:45 +0100

    Add methods for multiple HDB++ databases

--- PyTangoArchiving/hdbpp/migrate.py
+++ PyTangoArchiving/hdbpp/migrate.py
@@ -13,24 +13,41 @@ def get_archivers_filters(archiver='archiving/es/*'):
                     archiver,'AttributeFilters').items()))
     return filters
 
-def get_current_attributes(schema='*'):
+def get_schema_attributes(schema='*'):
     rd = pta.Reader(schema)
     alls = rd.get_attributes(active=True)
     return alls
 
-def match_archivers_with_attributes(alls=[]):
+def get_hdbpp_databases():
+    cms = ft.get_class_devices('HdbConfigurationManager')
+    dbs = {}
+    for c in cms:
+        props = ['LibConfiguration','ArchiverList']
+        props = ft.get_database().get_device_property(c,props)
+        db = dict(t.split('=') for t in props['LibConfiguration'])['dbname']
+        dbs[db] = {c:None}
+        for a in props['ArchiverList']:
+            dbs[db][a] =  ft.get_device_property(a,'AttributeList')
+    return dbs
+    
+
+def match_attributes_and_archivers(attrs=[],archs='archiving/es/*'):
     """
     This method returns matching archivers for a list of attributes
     in simplename format (no tango host).
     
     It applies AttributeFilters as defined in Tango DB (sorted)
     """
-    alls = alls or get_current_attributes()
-    if not fn.isSequence(alls): alls = [alls]
+    if isString(attrs):
+        attrs = ft.find_attributes(attrs)
+    else:
+        attrs = attrs or get_schema_attributes('*')
+    
     devattrs = fn.dicts.defaultdict(set)
-    [devattrs[a.rsplit('/',1)[0]].add(a) for a in alls];    
+    [devattrs[a.rsplit('/',1)[0]].add(a) for a in attrs];    
 
-    filters = get_archivers_filters()
+    if isSequence(archs): archs = '(%s)'%')|('.join(archs)
+    filters = get_archivers_filters(archs)
     r = devattrs.keys()
     archattrs = {}
     
@@ -40,19 +57,19 @@ def match_archivers_with_attributes(alls=[]):
             df = k
         else:
             m = fn.filtersmart(r,v)
-            attrs = set(fn.join(*[devattrs[d] for d in m]))
-            if len(attrs):
-                print(k,len(attrs),sorted(set(i.split('/')[-2] for i in m)))
-                archattrs[k] = attrs
+            currattrs = set(fn.join(*[devattrs[d] for d in m]))
+            if len(currattrs):
+                print(k,len(currattrs),sorted(set(i.split('/')[-2] for i in m)))
+                archattrs[k] = currattrs
                 print('\n')
             r = [a for a in r if a not in m]
         if i == len(filters)-1:
             k = df
             m = r
-            attrs = fn.join(*[devattrs[d] for d in m])
+            currattrs = fn.join(*[devattrs[d] for d in m])
             if len(attrs):
-                print(k,len(attrs),sorted(set(i.split('/')[-2] for i in m)))
-                archattrs[k] = attrs
+                print(k,len(currattrs),sorted(set(i.split('/')[-2] for i in m)))
+                archattrs[k] = currattrs
             
     return archattrs
         
