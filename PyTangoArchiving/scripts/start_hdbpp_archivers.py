1b2f1582ddce626722e00480a5d71805a017b36c
srubio <srubio@cells.es>
2021-03-10 11:21:36 +0100

    8.16.2 refactor restart_attributes method and script

--- PyTangoArchiving/scripts/start_hdbpp_archivers.py
+++ PyTangoArchiving/scripts/start_hdbpp_archivers.py
@@ -6,24 +6,28 @@ import PyTangoArchiving as pta
 import sys
 dbs = sys.argv[1:] or pta.get_hdbpp_databases()
 
+# empty output to be used from cron
+
 for db in dbs:
     api = pta.api(db)
     devs = api.get_subscribers() 
     off = [d for d in devs if not fn.check_device(d)]
-    print('restart %s subscribers stopped in %s db' % (len(off),db))
-    api.start_devices(dev_list=off,do_restart=True,force=False)
+    if len(off):
+        print('restart %s subscribers stopped in %s db' % (len(off),db))
+        api.start_devices(dev_list=off,do_restart=True,force=False)
     
     devs = api.get_periodic_archivers() 
     poff = [d for d in devs if not fn.check_device(d)]
-    print('restart %s pollers stopped in %s db' % (len(poff),db))
-    api.start_devices(dev_list=poff,do_restart=True,force=False)
+    if len(poff):
+        print('restart %s pollers stopped in %s db' % (len(poff),db))
+        api.start_devices(dev_list=poff,do_restart=True,force=False)
     
     stopped = api.get_stopped_attributes()
-    print('restart %s attributes stopped in %s db' % (len(stopped),db))
-    
-    for a in stopped:
-        try:
-            api.restart_attribute(a)
-        except Exception as e:
-            print(e)
+    if len(stopped):
+        print('restart %s attributes stopped in %s db' % (len(stopped),db))
+        for a in stopped:
+            try:
+                api.restart_attribute(a)
+            except Exception as e:
+                print(e)
 
